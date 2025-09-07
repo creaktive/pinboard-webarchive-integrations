918590d12cf467cfb83d2e4da107081184db43f1
Stanislaw Pusep <creaktive@gmail.com>
2025-09-07 11:37:49 +0200

    retry

--- pinboard2webarchive.pl
+++ pinboard2webarchive.pl
@@ -13,19 +13,24 @@ use constant PROCESS_FROM => time - 3 * 86_400;
 my $ua = Mojo::UserAgent->new(inactivity_timeout => 0);
 my $from = Mojo::Date->new(PROCESS_FROM)->to_datetime;
 
+my $retries = 0;
 for my $item (reverse $ua->get(PINBOARD_RSS_URL)->result->dom('item')->@*) {
     my $link = $item->at('link')->text;
     my $date = $item->at('date')->text;
 
     next if $date lt $from;
 
-    my $result = $ua->get('https://web.archive.org/save/' . url_escape $link)->result;
+    my $result = eval { $ua->get('https://web.archive.org/save/' . url_escape $link)->result };
 
-    next if $result->code == 520;
-    next if $result->code == 523;
-    if ($result->is_error) {
-        sleep 300;
-        redo;
+    # next if $result->code == 520;
+    # next if $result->code == 523;
+    if (!$result || $result->is_error) {
+        if ($retries++ <= 5) {
+            sleep 60;
+            redo;
+        } else {
+            $retries = 0;
+        }
     }
 }
 
